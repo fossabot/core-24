9085268d5edc835b742efdf69b437a49376d4f16
Gavin Walsh <walsh.gavin@gmail.com>
2022-04-12 13:48:07 +0000

    feat: add leaflet

--- apps/watch/pages/index.tsx
+++ apps/watch/pages/index.tsx
@@ -3,6 +3,7 @@ import Typography from '@mui/material/Typography'
 import { useQuery, gql } from '@apollo/client'
 
 import { VideoList } from '../src/components/Videos/VideoList/VideoList'
+import { Countries } from '../src/components/Countries/Countries'
 import { GetVideoTag } from '../__generated__/GetVideoTag'
 import { VideoType } from '../__generated__/globalTypes'
 
@@ -29,6 +30,9 @@ function VideoPage(): ReactElement {
       <Typography variant="h2">
         {jfm1Data?.videoTag?.title.find((t) => t.primary)?.value}
       </Typography>
+      up
+      <Countries />
+      down
       <VideoList
         filter={{
           availableVariantLanguageIds: ['529'],
